ba3d0335f7e3efaddb28263abe9dc5687abd6ad4
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-09-30 10:55:32 +0700

    🚑 fix ui design system

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,28 @@
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { useFonts } from "expo-font";
-import { NotoSans_100Thin, NotoSans_400Regular, NotoSans_500Medium } from "@expo-google-fonts/noto-sans";
-import { Kanit_100Thin, Kanit_400Regular, Kanit_500Medium } from "@expo-google-fonts/kanit";
+import{ 
+  NotoSans_100Thin,
+  NotoSans_200ExtraLight,
+  NotoSans_300Light,
+  NotoSans_400Regular,
+  NotoSans_500Medium,
+  NotoSans_600SemiBold,
+  NotoSans_700Bold,
+  NotoSans_800ExtraBold,
+  NotoSans_900Black, 
+} from "@expo-google-fonts/noto-sans";
+import { 
+  Kanit_100Thin,
+  Kanit_200ExtraLight,
+  Kanit_300Light,
+  Kanit_400Regular,
+  Kanit_500Medium, 
+  Kanit_600SemiBold,
+  Kanit_700Bold,
+  Kanit_800ExtraBold,
+  Kanit_900Black,
+} from "@expo-google-fonts/kanit";
 import "../global.css";
 
 
@@ -12,11 +32,23 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded] = useFonts({
     NotoSans_100Thin,
+    NotoSans_200ExtraLight,
+    NotoSans_300Light,
     NotoSans_400Regular,
     NotoSans_500Medium,
+    NotoSans_600SemiBold,
+    NotoSans_700Bold,
+    NotoSans_800ExtraBold,
+    NotoSans_900Black, 
     Kanit_100Thin,
+    Kanit_200ExtraLight,
+    Kanit_300Light,
     Kanit_400Regular,
-    Kanit_500Medium,
+    Kanit_500Medium, 
+    Kanit_600SemiBold,
+    Kanit_700Bold,
+    Kanit_800ExtraBold,
+    Kanit_900Black,
   });
 
   useEffect(() => {
