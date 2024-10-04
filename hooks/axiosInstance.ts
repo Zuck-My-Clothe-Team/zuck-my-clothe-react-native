bed8a534fa73b3f4b57b47c7a3014fe73d04ab0a
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-10-04 22:37:28 +0700

    sync-auth

--- hooks/axiosInstance.ts
+++ hooks/axiosInstance.ts
@@ -7,5 +7,8 @@ export const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
   timeout: 5000,
   withCredentials: true,
-  headers: { Authorization: `Bearer ${accessToken}` },
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+    Origin: process.env.EXPO_PUBLIC_FRONTEND_URL,
+  },
 });
