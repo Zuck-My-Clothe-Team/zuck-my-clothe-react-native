1480e46180ff225f7c73f5d1d92e299b4d492325
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-10-11 13:36:47 +0700

    fix : android login

--- hooks/axiosInstance.ts
+++ hooks/axiosInstance.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const accessToken = AsyncStorage.getItem("accessToken");
-
-export const axiosInstance = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
-  timeout: 5000,
-  withCredentials: true,
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-    Origin: process.env.EXPO_PUBLIC_FRONTEND_URL,
-  },
-});
+export const axiosInstance = async () => {
+  try {
+    const token = await AsyncStorage.getItem("accessToken");
+    return axios.create({
+      baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
+      timeout: 5000,
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        origin: process.env.EXPO_PUBLIC_FRONTEND_URL,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating axios instance:", error);
+    throw error;
+  }
+};
