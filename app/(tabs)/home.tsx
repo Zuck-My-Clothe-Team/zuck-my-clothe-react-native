d3281e1195205cecb37b4c758f161c43b1ada944
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-11-03 01:43:46 +0700

    test google maps

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,18 +1,21 @@
-import { SplashScreen } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const Homepage = () => {
-  SplashScreen.preventAutoHideAsync();
-
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
   return (
-    <View className=" flex-1 h-full justify-around px-8 w-full">
-      <View className=" w-full px-4 flex flex-col gap-y-4">
-        <Text className="text-2xl font-notoBold text-center">
-          This is homepage
-        </Text>
-      </View>
+    <View>
+      <MapView
+        style={{ alignSelf: "stretch", height: "100%" }}
+        region={mapRegion}
+        provider={PROVIDER_GOOGLE}
+      />
     </View>
   );
 };
