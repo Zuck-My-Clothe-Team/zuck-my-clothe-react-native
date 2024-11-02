4ffe5c5a50821cc55b22d988378545f57fb6e33f
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-11-02 19:15:53 +0700

    fix tabbar on iphone se

--- components/navigation/TabBar.tsx
+++ components/navigation/TabBar.tsx
@@ -1,4 +1,11 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
@@ -14,6 +21,7 @@ interface TabbarProps {
 }
 
 const TabBar = ({ state, descriptors, navigation }: TabbarProps) => {
+  const screenHeight = Dimensions.get("window").height;
   const tabIcons = {
     home: <Ionicons name="home" size={28} />,
     activity: <FontAwesome5 name="shopping-basket" size={28} />,
@@ -23,9 +31,9 @@ const TabBar = ({ state, descriptors, navigation }: TabbarProps) => {
           style={styles.paymentButton}
           onPress={() => navigation.navigate("payment")}
         >
-          <Image source={Bubble} style={styles.bubble}/>
-          <Image source={ScanIcon} style={styles.scanner}/>
-          
+          <Image source={Bubble} style={styles.bubble} />
+          <Image source={ScanIcon} style={styles.scanner} />
+
           {/* <MaterialCommunityIcons name="line-scan" size={48} color="#3A82F7" /> */}
         </TouchableOpacity>
       </View>
@@ -35,73 +43,78 @@ const TabBar = ({ state, descriptors, navigation }: TabbarProps) => {
   };
 
   return (
-    <View style={styles.tabbar}>
-      {state.routes.map((route: { key: string | number; name: string; params: any; }, index: React.Key | null | undefined) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    <View style={screenHeight > 700 ? styles.tabbar : styles.iphoneSE}>
+      {state.routes.map(
+        (
+          route: { key: string | number; name: string; params: any },
+          index: React.Key | null | undefined
+        ) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-        const isFocused = state.index === index;
+          const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-        };
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+          };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: "tabLongPress",
-            target: route.key,
-          });
-        };
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
 
-        return (
-          <View style={styles.tab} key={index}>
-            <TouchableOpacity
-              style={styles.tab}
-              accessibilityRole="button"
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-            >
-              {route.name === "payment" ? (
-                tabIcons.payment
-              ) : (
-                <>
-                  <Text
-                    style={{
-                      color: isFocused ? "#2594E1" : "#696969",
-                    }}
-                  >
-                    {tabIcons[route.name as keyof typeof tabIcons]}
-                  </Text>
-                  <Text
-                    style={{
-                      ...styles.tabText,
-                      color: isFocused ? "#2594E1" : "#696969",
-                    }}
-                  >
-                    {label}
-                  </Text>
-                </>
-              )}
-            </TouchableOpacity>
-          </View>
-        );
-      })}
+          return (
+            <View style={styles.tab} key={index}>
+              <TouchableOpacity
+                style={styles.tab}
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+              >
+                {route.name === "payment" ? (
+                  tabIcons.payment
+                ) : (
+                  <>
+                    <Text
+                      style={{
+                        color: isFocused ? "#2594E1" : "#696969",
+                      }}
+                    >
+                      {tabIcons[route.name as keyof typeof tabIcons]}
+                    </Text>
+                    <Text
+                      style={{
+                        ...styles.tabText,
+                        color: isFocused ? "#2594E1" : "#696969",
+                      }}
+                    >
+                      {label}
+                    </Text>
+                  </>
+                )}
+              </TouchableOpacity>
+            </View>
+          );
+        }
+      )}
     </View>
   );
 };
@@ -117,11 +130,41 @@ const styles = StyleSheet.create({
     paddingBottom: 25,
     paddingTop: 10,
     paddingHorizontal: 15, // Increase horizontal padding
-    borderRadius: 30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
     shadowOpacity: 0.45,
     shadowRadius: 3.84,
     shadowColor: "#BDE2FF",
     elevation: 5,
+    ...Platform.select({
+      android: {
+        elevation: 25,
+        shadowColor: "#005ff7",
+        shadowRadius: 10,
+      },
+    }),
+  },
+  iphoneSE: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#fff",
+    paddingBottom: 5,
+    paddingTop: 10,
+    paddingHorizontal: 15, // Increase horizontal padding
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    shadowOpacity: 0.45,
+    shadowRadius: 3.84,
+    shadowColor: "#BDE2FF",
+    elevation: 5,
+    ...Platform.select({
+      android: {
+        elevation: 25,
+        shadowColor: "#005ff7",
+        shadowRadius: 10,
+      },
+    }),
   },
   tab: {
     flex: 1,
@@ -159,11 +202,11 @@ const styles = StyleSheet.create({
     width: 80,
     height: 80,
   },
-  scanner:{
+  scanner: {
     position: "absolute",
     top: 15,
     zIndex: 1,
     width: 40,
     height: 40,
-  }
+  },
 });
